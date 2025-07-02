4ce88ea81ffcb11384cdf88902eb1fb5879ef280
planetatech <dssodre07@gmail.com>
2025-07-02 14:32:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Criador de conteúdo mobile", "Tech todos os dias", "Segue no Insta!"];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 1500; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(type, newTextDelay + 250);
+});
